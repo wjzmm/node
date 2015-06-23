9d36a0f671085344bb93fab5a699ea3bdfb846c6
wjzmm <cz920902@gmail.com>
2015-06-23 23:24:00 +0800

    write some functions about mongodb CRUB, but fail to run the function due to env problem

--- node/blog/func/mongodb.js
+++ node/blog/func/mongodb.js
@@ -1,17 +1,59 @@
 var mongodb = require('mongoose');
-var db = mongoose.createConnection('mongodb://localhost/test) 
-/*db.open(function(error, client){
-	if (!error){
-		console.log("connect")
+var db = mongoose.createConnection('mongodb://127.0.0.1:27017/test');
+
+db.on("error", function(error){
+	console.log(error);
+});
+
+var mongooseSchema = new mongoose.Schema({
+	username : { type : String, default : "chaochao"},
+	age      : { type : Number}
+});
+mongooseSchema.methods.findByUserName = function(username, callback){
+	return this.model('mongoose').find({username: username}, callback);
+}
+mongooseSchema.methods.findByAge = function(age, callback){
+	return this.model('mongoose').find({age: age}, callback);}
+var mongooseModel = db.model('mongoose', mongooseSchema);
+var doc = {username : 'wang', age : 25};
+var mongooseEntity = new mongooseModel(doc);
+mongooseEntity.save(function(error){
+	if(error){
+		console.log(error);
 	}else{
-		console.log("connect failed")
+		console.log('saved successfully');
 	}
-	var collection = new mongodb.Collection('client', 'user');
-	collection.find(function(error, cursor){
-		cursor.each(function(error, doc){
-			if(doc){
-				console.log("name:" + doc.name + "age:" + doc.age );
-			}
-		});
-	});*/
+	db.close();
+});
+mongooseModel.update(conditions, update, options, callback);
+var conditions = {username: 'zhang'};
+var update = {$set: {age: 20}};
+var options = {upsert: true};
+mongooseModel.update(conditions, update, options, function(error){
+	if(error){
+		console.log(error);
+	}else{
+		console.log("update success");
+	}
+	db.close();
+});
+var criteria = {username: 'zhang'};
+var fields = {age: 1};
+var options = {};
+mongooseModel.find(criteria, fields, options, function(error, result){
+	if(error){
+		console.log(error);
+	}else{
+		console.log(result);
+	}
+	db.close();
+});
+var conditions = {username: wang};
+mongooseModel.remove(conditions, function(error){
+	if(error){
+		console.log(error);
+	}else{
+		console.log("delete success");
+	}
+	db.close();
 });
